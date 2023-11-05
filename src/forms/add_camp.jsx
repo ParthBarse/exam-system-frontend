02ceb401b0efdd2ed081520b73a46bb7f91edbf8
Mayank-Deshmukh <mayankdeshmukh703@gmail.com>
2023-11-05 12:08:09 +0530

    Mayank

--- src/forms/add_camp.jsx
+++ src/forms/add_camp.jsx
@@ -20,14 +20,13 @@ function Table() {
         <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
 
         <main>
-        <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-screen-xl mx-auto">
-        <div className="grid grid-cols-12 gap-6">
+        <div className="px-4 sm:px-6 lg:px-8 py-4 w-full max-w-screen-xl mx-auto">
         <div className="col-span-full xl:col-span-12 bg-white dark:bg-slate-800 shadow-lg rounded-sm border border-slate-200 dark:border-slate-700">
       <header className="px-5 py-4 border-b border-slate-100 dark:border-slate-700" style={{display:'flex', justifyContent:'space-between'}}>
         <h2 className="font-semibold text-slate-800 dark:text-slate-100">Add Camp</h2>
         <Link end to="/camp" className="bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded">Back to camp list</Link>
       </header>
-        <div className="p-6 shadow-lg border border-gray-300 rounded-lg">
+        <div className="p-3 shadow-lg border border-gray-300 rounded-lg">
                 <form className="space-y-2">
                     <label className="text-lg font-semibold">Camp Name</label>
                     <input type="text" name="campName" required className="w-full p-3 border rounded-lg text-gray-800 focus:ring focus:ring-blue-400" />
@@ -74,13 +73,11 @@ function Table() {
             <input type="checkbox" name="isActive" className="text-blue-500 focus:ring focus:ring-blue-400" />
             <label className="text-lg">Is Active</label>
             </div>
-
             <button type="submit" className="w-32 p-3 mt-4 bg-blue-500 text-white rounded-lg hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-400">
             Submit
             </button>
         </form>
         </div>
-          </div>
           </div>
           </div>
         </main>
