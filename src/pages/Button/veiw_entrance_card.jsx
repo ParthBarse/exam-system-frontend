6c05168fba05c418afac3d1795a78f1c6c3e643f
Mayank-Deshmukh <mayankdeshmukh703@gmail.com>
2023-11-14 12:00:16 +0530

    Mayank

--- src/pages/Button/veiw_entrance_card.jsx
+++ src/pages/Button/veiw_entrance_card.jsx
@@ -1,118 +1,126 @@
-import React from 'react';
-import pic from './favicon.png';
+import React from "react";
+import pic from "./favicon.png";
+import sign from "./user-avatar-32.png";
 
-const AdmitCard = () => {
+function AdmitCard() {
   return (
+    <div className="flex flex-col"> 
+    <button
+        onClick={(e) => window.print()}
+        className="no-print bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded"
+      >
+        Print
+      </button>     
+      <section className="bg-gray-100 flex items-center justify-center max-w-4xl mx-auto p-2">
+        <div className="w-full bg-white shadow-md p-8 rounded-md">
+          <div className="admit-card border-4 p-4 mb-4">
+            <div className="BoxA border-1 p-4 mb-4">
+              <div className="flex justify-between">
+              <div className="w-1/4 flex items-center mr-16">
+                  <h5 className="text-xl font-bold">ENTRANCE_CARD</h5>
+                </div>
+                <div className="w-3/4 flex justify-around ml-14">
+                  <h5 className="text-4xl font-bold p-2 border">MARSHAL CADET FORCE</h5>
+                  <p className="font-xl">
+                   
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="BoxC border-1 p-4 mb-4">
+              <div className="flex">
+                <div className="w-full flex justify-around">
+                  <h5 className="font-bold">Enrollment No : 9910101</h5>
+                </div>
+              </div>
+            </div>
+            <div className="BoxD border-1 p-4 mb-4">
+              <div className="flex">
+                <div className="w-1/2">
+                <table className="table table-bordered w-full border-collapse">
+  <tbody>
+    <tr>
+      <td className="font-bold border p-2">
+        Student Name : <span className="font-normal">Vinod Sharma</span>{" "}
+      </td>
+    </tr>
+    <tr>
+      <td className="font-bold border p-2">
+        Sex : <span className="font-normal">Male</span>
+      </td>
+    </tr>
+    <tr>
+      <td className="font-bold border p-2">
+        Father Name : <span className="font-normal">S S Sharma</span>
+      </td>
+    </tr>
+    <tr>
+      <td className="font-bold border p-2">
+        ENROLLMENT NO : <span className="font-normal">99887766</span>{" "}
+      </td>
+    </tr>
+    <tr>
+      <td colSpan="2" className="font-bold border p-2">
+        Address : <span className="font-normal">Viman Nagar</span>
+      </td>
+    </tr>
+    <tr>
+      <td className="border p-2">
+        <b>Course: </b> B.TECH
+      </td>
+    </tr>
+  </tbody>
+</table>
 
-    <div className='flex flex-col'>
-      <button onClick={e=>window.print()} className="no-print bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded">Print</button>
-
-
-    <section className="bg-gray-100 flex items-center justify-center max-w-4xl mx-auto p-4">
-      <div className="w-full bg-white shadow-md p-8 rounded-md">
-        <div className="admit-card border-2 p-4 mb-4">
-          <div className="BoxA border-1 p-4 mb-4">
-            <div className="flex justify-between">
-              <div className="w-1/3">
-                <h5 className="text-xl font-bold">MARSHAL CADET FORCE</h5>
-                <p className="mt-2">Flat No.1, Punyodhaya Apt, Near Jagtap Dairy, <br />Aundh -Wakad Rd, Maharashtra, India</p>
-              </div>
-              <div className="w-1/3 flex items-center justify-center">
-                <img src={pic} className="w-16 h-16" alt="University Logo" />
-              </div>
-              <div className="w-1/3 flex items-center justify-end">
-                <h5 className="text-xl font-bold">ENTRANCE Card</h5>
+                </div>
+                <div className="w-1/2 flex flex-col items-center justify-center">
+                  <img
+                    src={pic}
+                    className="w-42 h-44 mb-2 border"
+                    alt="Student Photo"
+                  />
+                  <img src={""} className="p-2 border" alt="Signature" />
+                </div>
               </div>
             </div>
-          </div>
-          <div className="BoxC border-1 p-4 mb-4">
-            <div className="flex">
-              <div className="w-1/2">
-                <h5>Enrollment No : 9910101</h5>
+            <div className="BoxE border-1 p-2 mb-4 text-center">
+              <div className="flex">
+                <div className="w-full">
+                  <h5>PICK-UP POINT : pOINT HERE , cITY , INDIA</h5>
+                  {/* <p> point here <br />city ,  INDIA</p> */}
+                </div>
               </div>
             </div>
-          </div>
-          <div className="BoxD border-1 p-4 mb-4">
-            <div className="flex">
-              <div className="w-3/4">
-                <table className="table table-bordered w-full">
-                  <tbody>
-                    <tr>
-                      <td className="font-bold">ENROLLMENT NO : 9910101</td>
-                      <td><b>Course: </b> B.TECH</td>
-                    </tr>
-                    <tr>
-                      <td className="font-bold">Student Name: </td>
-                      <td>Vinod Sharma</td>
-                    </tr>
-                    <tr>
-                      <td className="font-bold">Sex: </td>
-                      <td>M</td>
-                    </tr>
-                    <tr>
-                      <td className="font-bold">Father/Husband Name: </td>
-                      <td>SS Sharma</td>
-                    </tr>
-                    <tr>
-                      <td colSpan="2" className="font-bold">Address: moh hasnxgxums</td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-              <div className="w-1/4 flex flex-col items-center justify-center">
-                <img src={pic} className="w-32 h-44 mb-2" alt="Student Photo" />
-                <img src alt="Signature" />
+            <div className="BoxF border-1 p-2 mb-4 text-center">
+              <div className="flex">
+                <div className="w-full">
+                  <table className="table table-bordered w-full">
+                    <thead>
+                      <tr>
+                        <th>Sr. No.</th>
+                        <th>Subject/Paper</th>
+                        <th>Exam Date</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>1</td>
+                        <td>English</td>
+                        <td>5 July 2019</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
               </div>
             </div>
+            <footer className="text-center mt-2">
+              <p className="text-gray-600">***************************** MARSHAL CADET FORCE *****************************</p>
+            </footer>
           </div>
-          <div className="BoxE border-1 p-4 mb-4 text-center">
-            <div className="flex">
-              <div className="w-full">
-                <h5>EXAMINATION VENUE</h5>
-                <p>NH - 79 Gangrar Chittorgarh - 312901 <br /> RAJASTHAN, INDIA</p>
-              </div>
-            </div>
-          </div>
-          <div className="BoxF border-1 p-4 mb-4 text-center">
-            <div className="flex">
-              <div className="w-full">
-                <table className="table table-bordered w-full">
-                  <thead>
-                    <tr>
-                      <th>Sr. No.</th>
-                      <th>Subject/Paper</th>
-                      <th>Exam Date</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td>1</td>
-                      <td>English</td>
-                      <td>5 July 2019</td>
-                    </tr>
-                    <tr>
-                      <td>2</td>
-                      <td>English</td>
-                      <td>5 July 2019</td>
-                    </tr>
-                    <tr>
-                      <td>3</td>
-                      <td>English</td>
-                      <td>5 July 2019</td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          </div>
-          <footer className="text-center">
-            <p className="text-gray-600">*** MEWAR UNIVERSITY ***</p>
-          </footer>
         </div>
-      </div>
-    </section>
+      </section>
     </div>
   );
-};
+}
 
 export default AdmitCard;
