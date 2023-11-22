826c65e4491c9e9a8c4005f6502d2467a7bbb0b9
Mayank-Deshmukh <mayankdeshmukh703@gmail.com>
2023-11-22 14:52:42 +0530

    Mayank Entrance Card made dynamic and added new template

--- src/pages/Button/veiw_entrance_card.jsx
+++ src/pages/Button/veiw_entrance_card.jsx
@@ -1,148 +1,203 @@
 import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
-import pic from "./favicon.png";
-import sign from "./user-avatar-32.png";
 import axios from "axios";
-import { useLocation } from 'react-router-dom';
+import { useLocation } from "react-router-dom";
+import './entrance.css';
 
 
 function AdmitCard() {
-
-
-  const [data,setData] = useState({})
-  const [id,setId] = useState('')
+  const [data, setData] = useState({});
+  const [id, setId] = useState("");
   const location = useLocation();
 
-  useEffect(()=>{
+  useEffect(() => {
     const queryParams = new URLSearchParams(location.search);
-    const uid = queryParams.get('id');
-    setId(uid)
-    axios.get(`https://mcf-backend.vercel.app/api/getStudent/${id}`).then(x=>setData(x.data))
-  },[location.search,id])
+    const uid = queryParams.get("id");
+    setId(uid);
+    axios
+      .get(`https://mcf-backend.vercel.app/api/getStudent/${id}`)
+      .then((x) => setData(x.data));
+  }, [location.search, id]);
 
   return (
-    <div className="flex flex-col">
+    <div className="">
+      <div class="box">
+        <div class="header">
+          <img
+            src="https://source.unsplash.com/random/180x80"
+            alt="logo"
+            class="logo"
+          />
+        </div>
+
+        <div class="inner-box">
+          <p>
+            Dear<strong> &nbsp; {data.First}</strong>
+          </p>
+          <img
+            src="https://source.unsplash.com/random/80x100"
+            alt="photo"
+            class="photo"
+          />
+        </div>
+
+        <div class="table-container">
+          <table class="table">
+            <tr>
+              <td>Registration No.</td>
+              <td>
+                <strong>{data.uuid}</strong>
+              </td>
+              <td>Chess No.</td>
+              <td>
+                <strong>{data.Chess}not added</strong>
+              </td>
+            </tr>
+            <tr>
+              <td>Name :</td>
+              <td colspan="3">{data.First + " " + data.last}</td>
+            </tr>
+            <tr>
+              <td>Camp Name :</td>
+              <td>{data.Camp}</td>
+              <td>Batch No :</td>
+              <td>ATC23-5</td>
+            </tr>
+            <tr>
+              <td>Total days :</td>
+              <td>7 Days</td>
+              <td>Company Name :</td>
+              <td>{data.CQY} not added</td>
+            </tr>
+            <tr>
+              <td>Pick Up Point : </td>
+              <td>{data.Pick_Up_Point}</td>
+              <td>Pick Up Time :</td>
+              <td>{data.Pick_Time}not added</td>
+            </tr>
+            <tr>
+              <td>In-chanrge Name :</td>
+              <td colspan="3">{data.incharge}not added</td>
+            </tr>
+          </table>
+        </div>
+
+        <div class="in-box-2">
+          <strong>Other Information :- </strong>
+        </div>
+
+        <div class="table-container">
+          <table class="table">
+            <tr>
+              <td>Camp Place :</td>
+              <td>
+                <strong>{data.venue}</strong>
+              </td>
+              <td>Camp Date :</td>
+              <td>
+                <strong>{data.Start_date + " - " + data.end_date}</strong>
+              </td>
+            </tr>
+            <tr>
+              <td>Guardian name :</td>
+              <td colspan="3">{data.parents}</td>
+            </tr>
+            <tr>
+              <td>Address :</td>
+              <td>{data.Address}</td>
+              <td>Landmark :</td>
+              <td>{data.Landmark}</td>
+            </tr>
+            <tr>
+              <td>City :</td>
+              <td>{data.city}</td>
+              <td>District :</td>
+              <td>{data.District}</td>
+            </tr>
+            <tr>
+              <td>State :</td>
+              <td>{data.State}</td>
+              <td>Pincode :</td>
+              <td>{data.Pincode}</td>
+            </tr>
+            <tr>
+              <td>E-mail :</td>
+              <td>{data.mail}not added</td>
+              <td>contact number :</td>
+              <td>{data.contact}not added</td>
+            </tr>
+            <tr>
+              <td>Whatsapp :</td>
+              <td>{data.whatsapp}not added</td>
+              <td>Fathers number :</td>
+              <td>{data.father_number}not added</td>
+            </tr>
+            <tr>
+              <td>Blood group :</td>
+              <td>{data.blood}not added</td>
+              <td>Date of birth :</td>
+              <td>{data.dob}not added</td>
+            </tr>
+            <tr>
+              <td>School Name :</td>
+              <td colspan="3">{data.school}not added</td>
+            </tr>
+          </table>
+        </div>
+
+        <div class="signature">
+          <div class="d-flex">
+            <p>sign</p>
+            <img src="https://source.unsplash.com/random/50x30" alt="" />
+            <p>Camp Commandant</p>
+            <p>MCF</p>
+          </div>
+        </div>
+
+        <div class="terms">
+          <div class="headline">
+            <strong>Terms & Conditions</strong>
+          </div>
+          <div class="list">
+            <ol>
+              <li>Without this Card the Entrance will not be accepted.</li>
+              <li>Card will not be accepted if it gets damaged.</li>
+              <li>
+                Pick Point given is Fixed, Other than these students will not be
+                picked up.
+              </li>
+              <li>Time schedule given is subject to be changed.</li>
+              <li>
+                If you have any query regarding Pick up Point or want any other
+                pick up then contact on the below number.
+              </li>
+              <li>
+                Bring Medical/Fitness Certificate on the first day of Camp,
+                dated must be before a maximum of 5 Days.
+              </li>
+              <li>Without Medical Certificate entry will be prohibited.</li>
+            </ol>
+          </div>
+        </div>
+
+        <div class="footer">
+          <p>
+            Mail us on: <strong>mcfcamp@gmail.com</strong>{" "}
+            (mailto:mcfcamp@gmail.com)
+          </p>
+          <p>
+            Cantact Us on : 9604082000/9604084000 / Website URL :
+            www.mcfcamp.com (http://mcfcamp.in)
+          </p>
+        </div>
+      </div>
       <button
         onClick={(e) => window.print()}
-        className="no-print bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded"
+        className="no-print bg-blue-500 hover:bg-blue-700 text-white py-1 px-80 mt-4 rounded"
       >
         Print
       </button>
-      <section className="bg-gray-100 flex items-center justify-center max-w-4xl mx-auto p-2">
-        <div className="w-full bg-white shadow-md p-8 rounded-md">
-          <div className="admit-card border-4 p-4 mb-4">
-            <div className="BoxA border-1 p-4 mb-4">
-              <div className="flex justify-between">
-                <div className="w-1/4 flex items-center mr-16">
-                  <h5 className="text-xl font-bold">ENTRANCE_CARD</h5>
-                </div>
-                <div className="w-3/4 flex justify-around ml-14">
-                  <h5 className="text-4xl font-bold p-2 border">
-                    MARSHAL CADET FORCE
-                  </h5>
-                  <p className="font-xl"></p>
-                </div>
-              </div>
-            </div>
-            <div className="BoxC border-1 p-4 mb-4">
-              <div className="flex">
-                <div className="w-full flex justify-around">
-                  <h5 className="font-bold">Enrollment No : 9910101</h5>
-                </div>
-              </div>
-            </div>
-            <div className="BoxD border-1 p-4 mb-4">
-              <div className="flex">
-                <div className="w-1/2">
-                  <table className="table table-bordered w-full border-collapse">
-                    <tbody>
-                      <tr>
-                        <td className="font-bold border p-2">
-                          Student Name :{" "}
-                          <span className="font-normal">{data.First + " " + data.last}</span>{" "}
-                        </td>
-                      </tr>
-                      <tr>
-                        <td className="font-bold border p-2">
-                          Sex : <span className="font-normal">Female</span>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td className="font-bold border p-2">
-                          Father Name :{" "}
-                          <span className="font-normal">{data.parents}</span>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td className="font-bold border p-2">
-                          ENROLLMENT NO :{" "}
-                          <span className="font-normal">{data.uuid}</span>{" "}
-                        </td>
-                      </tr>
-                      <tr>
-                        <td colSpan="2" className="font-bold border p-2">
-                          Address :{" "}
-                          <span className="font-normal">{data.Address}</span>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td className="border p-2">
-                          <b>Course: </b> B.TECH
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
-                <div className="w-1/2 flex flex-col items-center justify-center">
-                  <img
-                    src={pic}
-                    className="w-42 h-44 mb-2 border"
-                    alt="Student Photo"
-                  />
-                  <img src={""} className="p-2 border" alt="Signature" />
-                </div>
-              </div>
-            </div>
-            <div className="BoxE border-1 p-2 mb-4 text-center">
-              <div className="flex">
-                <div className="w-full">
-                  <h5>PICK-UP POINT : pOINT HERE , cITY , INDIA</h5>
-                  {/* <p> point here <br />city ,  INDIA</p> */}
-                </div>
-              </div>
-            </div>
-            <div className="BoxF border-1 p-2 mb-4 text-center">
-              <div className="flex">
-                <div className="w-full">
-                  <table className="table table-bordered w-full">
-                    <thead>
-                      <tr>
-                        <th>Sr. No.</th>
-                        <th>Subject/Paper</th>
-                        <th>Exam Date</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td>1</td>
-                        <td>English</td>
-                        <td>5 July 2019</td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
-              </div>
-            </div>
-            <footer className="text-center mt-2">
-              <p className="text-gray-600">
-                ***************************** MARSHAL CADET FORCE
-                *****************************
-              </p>
-            </footer>
-          </div>
-        </div>
-      </section>
     </div>
   );
 }
