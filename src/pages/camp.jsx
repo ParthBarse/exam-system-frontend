5afa4cb2f470a8673bf52c80e882f3a8660637fd
Mayank-Deshmukh <mayankdeshmukh703@gmail.com>
2023-11-24 18:03:44 +0530

    receipt template

--- src/pages/camp.jsx
+++ src/pages/camp.jsx
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import Sidebar from '../partials/Sidebar';
-import Header from '../partials/Header';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Sidebar from "../partials/Sidebar";
+import Header from "../partials/Header";
+import { Link } from "react-router-dom";
 
 function Camp() {
   const [data, setData] = useState([]);
@@ -12,13 +12,13 @@ function Camp() {
   useEffect(() => {
     // Fetch data from the API endpoint
     axios
-      .get('https://mcf-backend.vercel.app/api/getAllCamps')
+      .get("https://mcf-backend.vercel.app/api/getAllCamps")
       .then((response) => {
         // Update the state with the fetched data
         setData(response.data);
       })
       .catch((error) => {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       });
   }, []);
 
@@ -46,14 +46,28 @@ function Camp() {
           <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-screen-xl mx-auto">
             <div className="grid grid-cols-12 gap-6">
               <div className="col-span-full xl:col-span-12 bg-white dark:bg-slate-800 shadow-lg rounded-sm border border-slate-200 dark:border-slate-700">
-                <header className="px-5 py-4 border-b border-slate-100 dark:border-slate-700" style={{ display: 'flex', justifyContent: 'space-between' }}>
-                  <h2 className="font-semibold text-slate-800 dark:text-slate-100">Active Camps</h2>
-                  <Link end to="/add-camp" className="bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded">Add Camp</Link>
+                <header
+                  className="px-5 py-4 border-b border-slate-100 dark:border-slate-700"
+                  style={{ display: "flex", justifyContent: "space-between" }}
+                >
+                  <h2 className="font-semibold text-slate-800 dark:text-slate-100">
+                    Active Camps
+                  </h2>
+                  <Link
+                    end
+                    to="/add-camp"
+                    className="bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded"
+                  >
+                    Add Camp
+                  </Link>
                 </header>
                 <div className="p-4">
                   {/* Table */}
                   <div className="overflow-x-auto">
-                    <table className="dark:text-slate-300" style={{ width: '100%' }}>
+                    <table
+                      className="dark:text-slate-300"
+                      style={{ width: "100%" }}
+                    >
                       {/* Table header */}
                       <thead className="text-xs uppercase text-slate-400 dark:text-slate-500 bg-slate-50 dark:bg-slate-700 dark:bg-opacity-50 rounded-sm">
                         <tr>
@@ -61,19 +75,29 @@ function Camp() {
                             <div className="font-semibold text-left">Sr.</div>
                           </th>
                           <th className="p-2">
-                            <div className="font-semibold text-center">Name</div>
+                            <div className="font-semibold text-center">
+                              Name
+                            </div>
                           </th>
                           <th className="p-2">
-                            <div className="font-semibold text-center">venue</div>
+                            <div className="font-semibold text-center">
+                              venue
+                            </div>
                           </th>
                           <th className="p-2">
-                            <div className="font-semibold text-center">status</div>
+                            <div className="font-semibold text-center">
+                              status
+                            </div>
                           </th>
                           <th className="p-2">
-                            <div className="font-semibold text-center">fees</div>
+                            <div className="font-semibold text-center">
+                              fees
+                            </div>
                           </th>
                           <th className="p-2">
-                            <div className="font-semibold text-center">Action</div>
+                            <div className="font-semibold text-center">
+                              Action
+                            </div>
                           </th>
                         </tr>
                       </thead>
@@ -81,30 +105,77 @@ function Camp() {
                       <tbody className="text-sm font-medium divide-y divide-slate-100 dark:divide-slate-700">
                         {/* Row */}
                         {currentEntries.map((item, index) => (
-                          <tr style={{ padding: '2px' }} key={item.id}>
+                          <tr style={{ padding: "2px" }} key={item.id}>
                             <td>
-                              <div className="text-left" style={{ fontWeight: 'bold' }}>
+                              <div
+                                className="text-left"
+                                style={{ fontWeight: "bold" }}
+                              >
                                 {indexOfFirstEntry + index + 1}
                               </div>
                             </td>
                             <td className="p-2">
                               <div className="flex items-right">
-                                <div className="text-slate-800 dark:text-slate-100">{item.Name}</div>
+                                <div className="text-slate-800 dark:text-slate-100">
+                                  {item.Name}
+                                </div>
                               </div>
                             </td>
                             <td className="p-2">
                               <div className="text-left">{item.Venue}</div>
                             </td>
                             <td className="p-2">
-                              <div className="text-center text-emerald-500">{item.Status}</div>
+                              <div
+                                className={`text-center ${
+                                  item.Status === "inactive"
+                                    ? "text-red-500"
+                                    : "text-emerald-500"
+                                }`}
+                              >
+                                {item.Status}
+                              </div>
                             </td>
                             <td className="p-2">
                               <div className="text-right">{item.Fees}</div>
                             </td>
                             <td className="p-3">
                               <div className="text-center">
-                                <Link to={`/fee-details?campname=${item.Name}`} className="text-sm text-white px-2 bg-yellow-500" style={{ padding: '3px', fontSize: '13px', marginLeft: '1px', marginRight: '2px' }}>fee details </Link>
-                                <Link to={`/batch-details`} className="text-sm text-white px-2 bg-indigo-500 " style={{padding:'3px', fontSize:'13px', marginLeft: '2px', marginRight: '1px' }}>Batch details</Link>
+                                <Link
+                                  to={`/fee-details?campname=${item.Name}`}
+                                  className="text-sm text-white px-2 bg-yellow-500"
+                                  style={{
+                                    padding: "3px",
+                                    fontSize: "13px",
+                                    marginLeft: "1px",
+                                    marginRight: "2px",
+                                  }}
+                                >
+                                  fee details{" "}
+                                </Link>
+                                <Link
+                                  to={`/batch-details`}
+                                  className="text-sm text-white px-2 bg-indigo-500 "
+                                  style={{
+                                    padding: "3px",
+                                    fontSize: "13px",
+                                    marginLeft: "2px",
+                                    marginRight: "1px",
+                                  }}
+                                >
+                                  Batch details
+                                </Link>
+                                <Link
+                                  to={`/edit-fee-details?campname=${item.Name}`}
+                                  className="text-sm text-white px-2 bg-red-500 rounded"
+                                  style={{
+                                    padding: "3px",
+                                    fontSize: "13px",
+                                    marginLeft: "2px",
+                                    marginRight: "1px",
+                                  }}
+                                >
+                                  Edit
+                                </Link>
                               </div>
                             </td>
                           </tr>
@@ -114,10 +185,15 @@ function Camp() {
                   </div>
                   {/* Pagination */}
                   <div className="flex justify-center mt-4">
-                    {Array.from({ length: Math.ceil(data.length / entriesPerPage) }, (_, i) => i + 1).map((pageNumber) => (
+                    {Array.from(
+                      { length: Math.ceil(data.length / entriesPerPage) },
+                      (_, i) => i + 1
+                    ).map((pageNumber) => (
                       <button
                         key={pageNumber}
-                        className={`mx-1 px-3 py-1 bg-blue-500 text-white rounded-full ${currentPage === pageNumber && 'bg-indigo-700'}`}
+                        className={`mx-1 px-3 py-1 bg-blue-500 text-white rounded-full ${
+                          currentPage === pageNumber && "bg-indigo-700"
+                        }`}
                         onClick={() => paginate(pageNumber)}
                       >
                         {pageNumber}
