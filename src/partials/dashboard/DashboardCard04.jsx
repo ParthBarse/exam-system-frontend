06f57f4096749c3750550c10ec40d4944a72a311
Mayank-Deshmukh <mayankdeshmukh703@gmail.com>
2023-12-02 18:14:20 +0530

    m

--- src/partials/dashboard/DashboardCard04.jsx
+++ src/partials/dashboard/DashboardCard04.jsx
@@ -1,21 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import axios from 'axios'; // Import Axios for making API requests
-import LineChart from '../../charts/LineChart01';
-import Icon from '../../images/icon-01.svg';
+import axios from 'axios';
+import Icon from '../../images/icon-03.svg';
 import EditMenu from '../../components/DropdownEditMenu';
 
 function DashboardCard04() {
-  const [CancelledStudentCount, setCancelledStudentCount] = useState(0);
+  const [refundedStudentCount, setRefundedStudentCount] = useState(0);
 
   useEffect(() => {
     // Fetch data from the API
     axios.get('https://mcf-backend.vercel.app/api/CancelledStudentCount')
       .then(response => {
-        setCancelledStudentCount(response.data.CancelledStudentCount);
+        setRefundedStudentCount(response.data.RefundedStudentCount);
       })
       .catch(error => {
         console.error('Error fetching data:', error);
+        // Implement error handling or retry logic if needed
       });
   }, []);
 
@@ -28,7 +28,7 @@ function DashboardCard04() {
           {/* Menu button */}
           <EditMenu align="right" className="relative inline-flex">
             <li>
-              <Link className="font-medium text-sm text-slate-600 dark:text-slate-300 hover:text-slate-800 dark:hover:text-slate-200 flex py-1 px-3" to="CanStudent">
+              <Link className="font-medium text-sm text-slate-600 dark:text-slate-300 hover:text-slate-800 dark:hover:text-slate-200 flex py-1 px-3" to="/CanStudent">
                 More Info
               </Link>
             </li>
@@ -37,7 +37,7 @@ function DashboardCard04() {
         <h2 className="text-lg font-semibold text-slate-800 dark:text-slate-100 mb-2">Refunded Students</h2>
         <div className="text-xs font-semibold text-slate-400 dark:text-slate-500 uppercase mb-1">till date</div>
         <div className="flex items-start">
-          <div className="text-3xl font-bold text-slate-800 dark:text-slate-100 mr-2">{CancelledStudentCount}</div>
+          <div className="text-3xl font-bold text-slate-800 dark:text-slate-100 mr-2">{refundedStudentCount}</div>
         </div>
       </div>
     </div>
--- src/partials/dashboard/DashboardCard05.jsx
+++ src/partials/dashboard/DashboardCard05.jsx
@@ -1,18 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import axios from 'axios'; // Import Axios for making API requests
-import LineChart from '../../charts/LineChart01';
+import axios from 'axios';
 import Icon from '../../images/icon-02.svg';
 import EditMenu from '../../components/DropdownEditMenu';
 
 function DashboardCard05() {
-  const [CancelledStudentCount, setCancelledStudentCount] = useState(0);
+  const [extendedStudentCount, setExtendedStudentCount] = useState(0);
 
   useEffect(() => {
     // Fetch data from the API
-    axios.get('https://mcf-backend.vercel.app/api/CancelledStudentCount')
+    axios.get('https://mcf-backend.vercel.app/api/ExtendedStudentsCount')
       .then(response => {
-        setCancelledStudentCount(response.data.CancelledStudentCount);
+        setExtendedStudentCount(response.data.extendedStudentsCount);
       })
       .catch(error => {
         console.error('Error fetching data:', error);
@@ -28,7 +27,7 @@ function DashboardCard05() {
           {/* Menu button */}
           <EditMenu align="right" className="relative inline-flex">
             <li>
-              <Link className="font-medium text-sm text-slate-600 dark:text-slate-300 hover:text-slate-800 dark:hover:text-slate-200 flex py-1 px-3" to="CanStudent">
+              <Link className="font-medium text-sm text-slate-600 dark:text-slate-300 hover:text-slate-800 dark:hover:text-slate-200 flex py-1 px-3" to="/extended-students-info">
                 More Info
               </Link>
             </li>
@@ -37,7 +36,7 @@ function DashboardCard05() {
         <h2 className="text-lg font-semibold text-slate-800 dark:text-slate-100 mb-2">Extended Students</h2>
         <div className="text-xs font-semibold text-slate-400 dark:text-slate-500 uppercase mb-1">till date</div>
         <div className="flex items-start">
-          <div className="text-3xl font-bold text-slate-800 dark:text-slate-100 mr-2">{CancelledStudentCount}</div>
+          <div className="text-3xl font-bold text-slate-800 dark:text-slate-100 mr-2">{extendedStudentCount}</div>
         </div>
       </div>
     </div>
