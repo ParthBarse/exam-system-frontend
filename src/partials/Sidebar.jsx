fbbf252e2bd12e890a0b8525d4dde6935b6f8fe6
Mayank-Deshmukh <mayankdeshmukh703@gmail.com>
2023-12-03 21:11:16 +0530

    mayank

--- src/partials/Sidebar.jsx
+++ src/partials/Sidebar.jsx
@@ -226,7 +226,7 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
                         </div>
                       </a>
                       <div className="lg:hidden lg:sidebar-expanded:block 2xl:block">
-                        <ul className={`pl-9 mt-1 ${!open && 'hidden'}`}>
+                        <ul className={`pl-9 mt-1`}>
                           <li className="mb-1 last:mb-0">
                             <NavLink
                               end
