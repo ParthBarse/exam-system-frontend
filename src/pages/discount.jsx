7b2b0e53db279c023790158f5175e2ce0532c789
vansh845 <103567533+vansh845@users.noreply.github.com>
2023-12-12 15:44:23 +0530

    u

--- src/pages/discount.jsx
+++ src/pages/discount.jsx
@@ -3,6 +3,7 @@ import Sidebar from '../partials/Sidebar';
 import Header from '../partials/Header';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 export default function Discount() {
     const navigate = useNavigate()
